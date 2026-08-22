f202db97e0a3403c02b3ed4852d1b4fa9f719ccc
serigio04 <serigi.gomar15@gmail.com>
2026-08-21 22:39:29 -0600

    [ADD] Se agregan archivos de estructura de angular

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
   ],
 };
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' } // Redirige por defecto al login
+];
