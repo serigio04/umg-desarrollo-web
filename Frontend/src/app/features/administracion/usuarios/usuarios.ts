62a5e00b5f7fa0d7f2a4ad9433fbcb9804493b6b
serigio04 <serigi.gomar15@gmail.com>
2026-08-26 20:42:04 -0600

    [ADD] Se agrega componente de usuarios para la vista del administrador

---
+++ Frontend/src/app/features/administracion/usuarios/usuarios.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-usuarios',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './usuarios.html',
+  styleUrl: './usuarios.css'
+})
+//todo quitar data mock
+export class UsuariosComponent {
+  usuarios = [
+    { id: 1, username: 'prueba_doctor', rol: 'ADMIN' },
+    { id: 2, username: 'operador_citas', rol: 'OPERADOR' }
+  ];
+}
