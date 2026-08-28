ef7efc4df548c9312ad478c04516d6b9ee024a8c
serigio04 <serigi.gomar15@gmail.com>
2026-08-27 23:57:22 -0600

    [ADD] Se agrega navbar a la estructura de la app

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend-app');
+  private router = inject(Router);
+
+  // Muestra el Navbar solo si NO estamos en la página de login
+  mostrarNavbar(): boolean {
+    return this.router.url !== '/login' && this.router.url !== '/';
+  }
 }
